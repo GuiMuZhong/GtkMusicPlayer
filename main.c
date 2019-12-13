b99e56be9a6d5969e5080700b1ae4fab9a7eecbf
CQW <chen_quanwei@qq.com>
2019-12-13 11:07:06 +0800

    image

--- main.c
+++ main.c
@@ -25,6 +25,8 @@ void deal_button(GtkWidget *button, gpointer user_data);
 void music_player();
 void* progress_speed_fun1(void * arg);
 void* progress_speed_fun2(void * arg);
+void chang_background(GtkWidget *widget, int w, int h, const gchar *path);
+void load_image(GtkWidget *image, const char *file_path, const int w, const int h );
 
 int main(int argc, char *argv[])
 {
@@ -141,54 +143,58 @@ void user_ui(int argc, char *argv[], int kid_pid)
 	gtk_init(&argc, &argv);
 	GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 	gtk_window_set_title(GTK_WINDOW(win), "CQW-Music");
-	gtk_window_set_default_size(GTK_WINDOW(win), 520, 520);	
+	gtk_window_set_default_size(GTK_WINDOW(win), 620, 520);	
 	g_signal_connect(win, "destroy", G_CALLBACK(deal_quit), str_kid_ped);
-	
+	chang_background(win, 620, 520, "./image/1.jpg");
+
 	// 网格布局
-	GtkWidget *table = gtk_table_new(5, 3, TRUE);
+	GtkWidget *table = gtk_table_new(20, 3, TRUE);
 	gtk_container_add(GTK_CONTAINER(win), table);
 
 	// 显示
 	label_music_name = gtk_label_new("");
-	gtk_table_attach_defaults(GTK_TABLE(table), label_music_name, 0, 3, 0, 1);
+	gtk_table_attach_defaults(GTK_TABLE(table), label_music_name, 0, 3, 0, 16);
 
 	// 进度条
 	progress_speed = gtk_progress_bar_new();
-	gtk_table_attach_defaults(GTK_TABLE(table), progress_speed, 0, 3, 1, 2);
+	gtk_table_attach_defaults(GTK_TABLE(table), progress_speed, 0, 3, 16, 17);
 	gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_speed), 0);
 	gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_speed), "SPEED");
 
 	// 声音
 	progress_voice = gtk_progress_bar_new();
-	gtk_table_attach_defaults(GTK_TABLE(table), progress_voice, 0, 3, 2, 3);
+	gtk_table_attach_defaults(GTK_TABLE(table), progress_voice, 0, 3, 17, 18);
 	gtk_progress_bar_set_fraction(GTK_PROGRESS_BAR(progress_voice), 0.5);
 	gtk_progress_bar_set_text(GTK_PROGRESS_BAR(progress_voice), "VOICE");
 	GtkWidget *btn_voice = gtk_button_new_with_label("Mute");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_voice, 1, 2, 3, 4);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_voice, 1, 2, 18, 19);
 	GtkWidget *btn_up = gtk_button_new_with_label("Up");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_up, 2, 3, 3, 4);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_up, 2, 3, 18, 19);
 	GtkWidget *btn_down = gtk_button_new_with_label("Down");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_down, 0, 1, 3, 4);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_down, 0, 1, 18, 19);
 
 	// 上一首 开始 下一首
 	GtkWidget *btn_last = gtk_button_new_with_label("Last");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_last, 0, 1, 4, 5);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_last, 0, 1, 19, 20);
 	GtkWidget *btn_start = gtk_button_new_with_label("Start");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_start, 1, 2, 4, 5);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_start, 1, 2, 19, 20);
 	GtkWidget *btn_next = gtk_button_new_with_label("Next");
-	gtk_table_attach_defaults(GTK_TABLE(table), btn_next, 2, 3, 4, 5);
+	gtk_table_attach_defaults(GTK_TABLE(table), btn_next, 2, 3, 19, 20);
 
+	// 进度条询问进程
 	pthread_t tid_progress_speed1;
 	pthread_create(&tid_progress_speed1, NULL, progress_speed_fun1, NULL);
 	pthread_detach(tid_progress_speed1);
 
+	// 进度条修改进程
 	pthread_t tid_progress_speed2;
 	pthread_create(&tid_progress_speed2, NULL, progress_speed_fun2, NULL);
 	pthread_detach(tid_progress_speed2);
 
-	pthread_t tid_voice;
-	pthread_create(&tid_voice, NULL, voice_control_fun, NULL);
-	pthread_detach(tid_voice);
+	// 语音进程
+	// pthread_t tid_voice;
+	// pthread_create(&tid_voice, NULL, voice_control_fun, NULL);
+	// pthread_detach(tid_voice);
 
 	g_signal_connect(btn_voice, "clicked", G_CALLBACK(deal_button), NULL);
 	g_signal_connect(btn_last, "clicked", G_CALLBACK(deal_button), NULL);
@@ -297,4 +303,41 @@ void music_player()
 				"-slave", "-quiet", "-idle", 
 				"-input", 
 				"file=./fifo_music_cmd", NULL);
+}
+
+void chang_background(GtkWidget *widget, int w, int h, const gchar *path)
+{
+	gtk_widget_set_app_paintable(widget, TRUE); //允许窗口可以绘图
+	gtk_widget_realize(widget);
+	/* 更改背景图时，图片会重叠
+	* 这时要手动调用下面的函数，让窗口绘图区域失效，产生窗口重绘制事件（即 expose 事件）。
+	*/
+	gtk_widget_queue_draw(widget);
+	GdkPixbuf *src_pixbuf = gdk_pixbuf_new_from_file(path, NULL); // 创建图片资源对象
+	// w, h是指定图片的宽度和高度
+	GdkPixbuf *dst_pixbuf = gdk_pixbuf_scale_simple(src_pixbuf, w, h,GDK_INTERP_BILINEAR);
+	GdkPixmap *pixmap = NULL;
+	/* 创建pixmap图像;
+	步骤3：设置按钮图片（函数直接调用load_image）
+	参数介绍：
+	* NULL：不需要蒙版;
+	* 123： 0~255，透明到不透明
+	*/
+	gdk_pixbuf_render_pixmap_and_mask(dst_pixbuf, &pixmap, NULL, 128);
+	// 通过pixmap给widget设置一张背景图，最后一个参数必须为: FASLE
+	gdk_window_set_back_pixmap(widget->window, pixmap, FALSE);
+	// 释放资源
+	g_object_unref(src_pixbuf);
+	g_object_unref(dst_pixbuf);
+	g_object_unref(pixmap);
+}
+
+void load_image(GtkWidget *image, const char *file_path, const int w, const int h )
+{
+	gtk_image_clear( GTK_IMAGE(image) ); // 清除图像
+	GdkPixbuf *src_pixbuf = gdk_pixbuf_new_from_file(file_path, NULL); // 创建图片资源
+	GdkPixbuf *dest_pixbuf = gdk_pixbuf_scale_simple(src_pixbuf, w, h, GDK_INTERP_BILINEAR); // 指定大小
+	gtk_image_set_from_pixbuf(GTK_IMAGE(image), dest_pixbuf); // 图片控件重新设置一张图片(pixbuf)
+	g_object_unref(src_pixbuf); // 释放资源
+	g_object_unref(dest_pixbuf); // 释放资源
 }
